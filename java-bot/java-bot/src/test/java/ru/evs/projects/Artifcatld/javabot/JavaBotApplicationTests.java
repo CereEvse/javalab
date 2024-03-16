5e79fa770583893e551edbd18daa24c59d48e591
CereEvse <mister.evsevev@mail.ru>
2024-03-16 13:36:50 +0300

    test

---
+++ java-bot/java-bot/src/test/java/ru/evs/projects/Artifcatld/javabot/JavaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.evs.projects.Artifcatld.javabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
